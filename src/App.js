b6757ad38aa38a87e6726f678c8b5cc51d4b5a51
Alcides Yunier Turruellas Osorio <ayunierto@gmail.com>
2022-08-29 12:53:57 -0500

    installing react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Home from './components/Home'
+import Login from './components/Login'
+import Register from './components/Register'
 
 function App() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+    <div className='bg-slate-300 h-screen text-white flex' >
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='login' element={<Login />} />
+        <Route path='register' element={<Register />} />
+      </Routes>
     </div>
   )
 }
