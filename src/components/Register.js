f048c9b221875fd8f28d76f06fed571f6b96e00f
Alcides Yunier Turruellas Osorio <ayunierto@gmail.com>
2022-08-29 16:39:02 -0500

    user registration

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,9 +1,66 @@
-import React from 'react'
+import { useState } from "react";
+import { useAuth } from "../context/authContext";
+import { useNavigate } from "react-router-dom";
 
 function Register() {
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  });
+
+  const { signup } = useAuth();
+
+  const navigate = useNavigate();
+
+  const [error, setError] = useState();
+
+  const handleChange = ({ target: { name, value } }) => {
+    setUser({ ...user, [name]: value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setError('');
+    try {
+      await signup(user.email, user.password);
+      navigate("/");
+    } catch (error) {
+      // Modificadno mensajes de errors
+      // if (error.code === "auth/internal-error") {
+      //   setError('Invalid email')
+      // }
+      setError(error.message);
+    }
+  };
+
   return (
-    <div>Register</div>
-  )
+    <div>
+      {error && <p>{error}</p>}
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="email">Email</label>
+
+        <input
+          type="email"
+          name="email"
+          id="email"
+          placeholder="youremail@company.com"
+          onChange={handleChange}
+        />
+
+        <label htmlFor="password">password</label>
+
+        <input
+          type="password"
+          name="password"
+          id="password"
+          placeholder="*********"
+          onChange={handleChange}
+        />
+
+        <button>Register</button>
+      </form>
+    </div>
+  );
 }
 
-export default Register
+export default Register;
--- src/context/authContext.js
+++ src/context/authContext.js
@@ -1,17 +1,19 @@
 import { createContext, useContext } from "react";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../firebase";
 
 export const authContext = createContext();
 
 export const useAuth = () => {
-    const context = useContext(authContext)
-    return context
-}
+  const context = useContext(authContext);
+  return context;
+};
 
 export function AuthProvider({ children }) {
+  const signup = (email, password) =>
+    createUserWithEmailAndPassword(auth, email, password);
 
-  const user = {
-    login: true,
-  };
-
-  return <authContext.Provider value={{ user }}>{children}</authContext.Provider>;
+  return (
+    <authContext.Provider value={{ signup }}>{children}</authContext.Provider>
+  );
 }
