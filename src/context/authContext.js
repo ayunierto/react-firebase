2521620d8ad6c36399b45c659a018c5b931a6582
Alcides Yunier Turruellas Osorio <ayunierto@gmail.com>
2022-08-30 07:56:36 -0500

    reset password

--- src/context/authContext.js
+++ src/context/authContext.js
@@ -1,5 +1,5 @@
 import { createContext, useContext, useEffect, useState } from "react";
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, signOut, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, signOut, GoogleAuthProvider, signInWithPopup, sendPasswordResetEmail } from "firebase/auth";
 import { auth } from "../firebase";
 
 export const authContext = createContext();
@@ -27,6 +27,9 @@ export function AuthProvider({ children }) {
     return signInWithPopup(auth, googleProvider)
   }
 
+  const resetPassword = (email) => sendPasswordResetEmail(auth, email)
+
+
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, currentUser => {
       setUser(currentUser)
@@ -36,6 +39,6 @@ export function AuthProvider({ children }) {
   }, [])
 
   return (
-    <authContext.Provider value={{ signup, signin, user, logout, loading, loginWithGoogle }}>{children}</authContext.Provider>
+    <authContext.Provider value={{ signup, signin, user, logout, loading, loginWithGoogle, resetPassword }}>{children}</authContext.Provider>
   );
 }
